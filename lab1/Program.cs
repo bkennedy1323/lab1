93fb646ab2a0fdbcff25e4c0175b76f7ba145d52
bkennedy1323 <123656133+bkennedy1323@users.noreply.github.com>
2025-03-24 02:17:53 +0000

    inserted const vars and string date

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string name = "Blake"; 
+        Console.WriteLine("Hello, " + name + "!");
     }
 }
