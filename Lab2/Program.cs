0394cb96fb5b8e9fc4e9eaa3429891a86975cb16
bkennedy1323 <123656133+bkennedy1323@users.noreply.github.com>
2025-05-11 12:05:56 +0000

    initial

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int myfav = 30;
+        double mydub = 3.14;
+        char myletter = 'B';
+        bool mybool = true;
+        string mystr = "Hello";
+
+        Console.WriteLine("My favorite number is " + myfav + ".");
+        Console.WriteLine("My favorite decimal is " + mydub + ".");
+        Console.WriteLine("My favorite letter is " + myletter + ".");
+        Console.WriteLine("Yes it is " + mybool + " that I am from Arkansas!");
+        Console.WriteLine( + mystr + ", my name is Blake.");
     }
 }
--- lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48993409599c0120e30cf58e4a5aa1341de3334b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb794d7fe8e5752852fc54ef537973236321c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
