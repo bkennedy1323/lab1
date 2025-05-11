dd4d1c6bf89e418266e41bdf4dcc2267610c5a45
bkennedy1323 <123656133+bkennedy1323@users.noreply.github.com>
2025-05-11 14:46:07 +0000

    Final

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -4,6 +4,101 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       int a = 10;
+       int b = 5;
+       int c = 15;
+
+       Console.WriteLine(a > b);
+       Console.WriteLine(a < c);
+       Console.WriteLine(a > b && a > c);
+
+
+        
+       bool isRaining = true;
+       bool haveUmbrella = false;
+
+       if (isRaining || haveUmbrella)
+       {
+        Console.WriteLine("It is raining, take an umbrella.");
+       }
+        else if (!isRaining)
+      {
+       Console.WriteLine("It is not raining, you're good to go!");
+
+      }
+
+       
+       else
+       {
+        Console.WriteLine("It is not raining, you're good to go!");
+       }
+
+
+       Console.WriteLine("Please enter your age:");
+       int age = Convert.ToInt32(Console.ReadLine());
+
+       if (age < 5)
+       {
+        Console.WriteLine("Your ticket is free!");
+       }
+       else if (age > 5 && age < 12)
+       {
+         Console.WriteLine("Child ticket: $5");
+
+       }
+       else if (age > 13 && age < 64)
+       {
+        Console.WriteLine("Standard ticket price: $12");
+       }
+
+       else if (age > 65)
+       {
+        Console.WriteLine("Senior ticket price: $6");
+       }
+
+       Console.WriteLine("What day is it?");
+       string day = Console.ReadLine();
+
+       switch(day)
+       { 
+        case "Monday":
+        Console.WriteLine("It's Monday.");
+        break;
+        
+        case "Tuesday":
+        Console.WriteLine("It's Tuesday.");
+        break;
+        
+        case "Wednesday":
+        Console.WriteLine("It's Wednesday.");
+        break;
+
+        case "Thursday":
+        Console.WriteLine("It's Thursday.");
+        break;
+
+        case "Friday":
+        Console.WriteLine("It's Friday.");
+        break;
+
+        case "Saturday":
+        Console.WriteLine("It's Saturday.");
+        break;
+
+        case "Sunday":
+        Console.WriteLine("It's Sunday.");
+        break;
+
+        default:
+        Console.WriteLine(day + " is not a day!");
+        break;
+
+       }
+
+
     }
+
+
+       
+
 }
--- lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb794d7fe8e5752852fc54ef537973236321c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6be0f85ceabac538a97933d72a5aeab9bba315c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
