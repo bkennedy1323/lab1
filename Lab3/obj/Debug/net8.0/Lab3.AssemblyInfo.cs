8f5e73d792469cc100028f5e22aa6fd03cc46fc5
bkennedy1323 <123656133+bkennedy1323@users.noreply.github.com>
2025-05-11 17:54:36 +0000

    final

--- Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6be0f85ceabac538a97933d72a5aeab9bba315c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4d1c6bf89e418266e41bdf4dcc2267610c5a45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lab4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      for (int l = 1; l <= 10; l++) 
+{
+  Console.WriteLine(l);
+}
+
+    for (int l = 1; l <= 20; l++)
+     
+    {
+        if (l % 2 == 0)
+        Console.WriteLine(l);
+    }
+
+    for (int l = 5; l >= 0; l--)
+    
+    {
+        Console.WriteLine(l);
+    }
+
+    
+     double number, total = 100;
+     Console.WriteLine("Enter a number greater than 100: ");
+   
+   
+    do 
+    {
+       Console.Write("Number: ");
+       number = Convert.ToDouble(Console.ReadLine());
+       total += number;
+
+    }
+    while (number < 100);
+
+    
+
+   
+            for (int b = 10; b <= 1000; b++)
+     
+    {
+        if (b % 10 == 0)
+        Console.WriteLine(b);
+    }
+
+
+     int i = '*';
+     while (i >= 1)
+     {
+       int k = i;
+       while (k >= 1)
+       {
+        Console.Write(k + " ");
+        k++;
+       }
+       Console.WriteLine();
+       i++;
+
+     }
+
+
+
+            
+        }
+
+ 
+    
+
+
+    
+    
+        
+    
+   
+
+}
